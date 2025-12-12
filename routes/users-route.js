dd80938e12a5f1b59351437b597f7f6ae5a78f98
Myungjun Ko <mko417@terpmail.umd.edu>
2025-12-12 16:06:36 -0500

    display profiles

--- routes/users-route.js
+++ routes/users-route.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
 router.get("/", (req, res) => {
-  res.send("Users list route working");
+  User.find().sort({createdAt: -1})
+  .then(users => {
+    res.render("user-list", { users });
+  })
+  .catch(err => {
+    console.error(err);
+    res.send("Error loading user list.");
+  });
 });
 
 module.exports = router;
